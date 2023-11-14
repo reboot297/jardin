fab0767045484ae42039cc7f511035cd3d5b519b
Reboot297 <reboot297@gmail.com>
2023-11-14 17:35:02 +0200

    create empty app

---
+++ demo/build.gradle.kts
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023. Viktor Pop
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("io.gitlab.arturbosch.detekt")
+}
+apply {
+    from("$rootDir/tools/script-check.gradle")
+}
+
+android {
+    namespace = "com.reboot297.jardin"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.reboot297.jardin"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+}
