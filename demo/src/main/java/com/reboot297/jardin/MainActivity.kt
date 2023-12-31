5146631ed9b782e017eb084c7326e0639a24c56d
reboot297 <reboot297@gmail.com>
2023-12-31 20:44:55 +0200

    info: added features info

--- demo/src/main/java/com/reboot297/jardin/MainActivity.kt
+++ demo/src/main/java/com/reboot297/jardin/MainActivity.kt
@@ -45,6 +45,28 @@ class MainActivity : AppCompatActivity() {
         Item("Battery Status") { Info(applicationContext).batteryStatus().print() },
         Item("") { },
 
+        Item("Features Full") { Info(applicationContext).features().print() },
+        Item("Features Audio") { Info(applicationContext).featuresAudio().print() },
+        Item("Features Biometrics") { Info(applicationContext).featuresBiometrics().print() },
+        Item("Features Bluetooth") { Info(applicationContext).featuresBluetooth().print() },
+        Item("Features Camera") { Info(applicationContext).featuresCamera().print() },
+        Item("Features DeviceType") { Info(applicationContext).featuresDeviceType().print() },
+        Item("Features Graphics") { Info(applicationContext).featuresGraphics().print() },
+        Item("Features Location") { Info(applicationContext).featuresLocation().print() },
+        Item("Features NFC") { Info(applicationContext).featuresNFC().print() },
+        Item("Features Sensors") { Info(applicationContext).featuresSensors().print() },
+        Item("Features Telephony") { Info(applicationContext).featuresTelephony().print() },
+        Item("Features Touch") { Info(applicationContext).featuresTouch().print() },
+        Item("Features VR") {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                Info(applicationContext).featuresVR().print()
+            } else {
+                Toast.makeText(this, "Available since API 24", Toast.LENGTH_LONG).show()
+            }
+        },
+        Item("Features Wifi") { Info(applicationContext).featuresWifi().print() },
+        Item("") { },
+
         Item("Locales Full") { Info(applicationContext).locales().print() },
         Item("Locales Application") {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
